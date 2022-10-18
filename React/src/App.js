e0626f10e1f56ed037b5134f72d9f6c1d57d7952
Nicolly Evangelista Hernandes dos Santos <112009045+NicollySantos@users.noreply.github.com>
2022-10-18 17:58:25 -0300

    Exemplo

---
+++ React/src/App.js
@@ -0,0 +1,17 @@
+import Imagem from '../src/img/fundo.jpg';
+import './App.css';
+
+function App() {
+
+  return (
+    <div>
+      <article>
+        <img src={Imagem} />
+        <h2></h2>
+        <h3></h3>
+      </article>
+    </div>
+  );
+}
+
+export default App;
